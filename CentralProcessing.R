8457abb45462abe3471aa99c053bdf34c1212ead
Vojtech Huser <vojtech.huser@gmail.com>
2020-10-21 12:27:09 -0400

    Update CentralProcessing.R

--- CentralProcessing.R
+++ CentralProcessing.R
@@ -10,7 +10,11 @@ ll<-map(ll,~{names(.x)<-tolower(names(.x));return(.x)})
 ll2<-map2(ll,basename(ofiles),~mutate(.x,site=.y))
 d<-bind_rows(ll2)
 
-
+# ab=d %>% mutate(s2=str_split_fixed(site,"-",2)[2])
+# 
+# str_split_fixed(d$site,"-",2)
+# 
+# str_split_fixed('site-ss',"-",2)$2
 
 #expand
 names(d)
@@ -23,6 +27,7 @@ d2<-d  %>%
             ,by=c('unit_concept_id'='concept_id'))
 
 
+
 names(d2)
 
 d2 %<>% arrange(site,measurement_concept_id,desc(count_value))
@@ -30,14 +35,16 @@ d2 %<>% group_by(measurement_concept_id,site) %>% mutate(site_ratio=count_value/
 d2 %<>% group_by(measurement_concept_id,unit_concept_id) %>% mutate(network_ratio=count_value/sum(count_value))
 
 
+d2 %<>% ungroup()
 d2 %>% write_csv('ah-d2.csv')
 
+networkRatios= d2 %>% select(measurement_concept_id,unit_concept_id,network_ratio) %>% distinct() 
 
 sites= d2 %>% count(site)
+sites
 
-tests= d2 %>% count(measurement_concept_id)
 names(d2)
-helper = d2 %>% ungroup() %>% select(measurement_concept_id,concept_name.x,site) %>%
+helper = d2  %>% select(measurement_concept_id,concept_name.x,site) %>%
   distinct() 
 
 testsSiteCnt=helper %>% group_by(measurement_concept_id,concept_name.x) %>% 
@@ -47,7 +54,7 @@ testsSiteCnt=helper %>% group_by(measurement_concept_id,concept_name.x) %>%
 
 
 testsNtwCnt=d2 %>% group_by(measurement_concept_id) %>% 
-  summarise(network_sum_count_value=sum(count_value)) 
+  summarise(network_sum_count_value_meas=sum(count_value)) 
 
 # testsFiltered=tests %>% filter(site_cnt>1) %>% left_join(testsNtwCnt)
 # testsFiltered %>% write_csv('ah-testsFiltered.csv')
@@ -56,28 +63,41 @@ testsNtwCnt=d2 %>% group_by(measurement_concept_id) %>%
 options(scipen=999) #disable scientific notation
 
 
-
-tuPairs=d2 %>% group_by(measurement_concept_id,unit_concept_id) 
-
-tuPairsCnt=d2 %>% group_by(measurement_concept_id,unit_concept_id,site) %>% mutate(site_ratio=count_value/sum(count)) 
-  site_cnt=n())
-tuSubset=tuPairs %>% filter(site_cnt>=2)
-
 names(d2)
+tuPairs=d2 %>% group_by(measurement_concept_id,concept_name.x,unit_concept_id,concept_name.y) %>% 
+  summarise(pair_cnt=n(),pair_sum_count_value=sum(count_value))
 
-tOverview=d2 %>% arrange(measurement_concept_id,desc(count_value)) %>% 
-group_by(measurement_concept_id,concept_name.x) %>% 
-  summarize(n=n()
-            ,unit_names=paste(concept_name.y,collapse = '|')
-            ,units_cids=paste(unit_concept_id,collapse = '|')) 
-tOverview %>% left_join(testsSiteCnt)
+tuPairs %<>% left_join(testsSiteCnt) %>% left_join(testsNtwCnt) 
+names(tuPairs)
+
+tuPairs %<>% mutate(network_ratio=pair_sum_count_value/network_sum_count_value_meas)
+tuSubset=tuPairs %>% filter(site_cnt>=2)  %>% filter(pair_cnt>1) %>% 
+  arrange(desc(pair_cnt))
+names(d2)
+names(tuPairs )
+#nice output one
+tuSubset %<>% select(1:7,10,9,8)
+tuSubset %>% write_csv('ah-tuSubset-test-unit-pairs-safe-subset.csv')
 
 
-tOverview %<>%   filter(n>1)
 
-tOverviewExt=tOverview %>% left_join(tests) %>%
-  left_join(testsNtwCnt) %>% 
-  arrange(desc(site_cnt),desc(network_sum_count_value),measurement_concept_id)
-names(tOverviewExt)
 
-tOverviewExt  %>% write_csv('ah-tOverviewExt.csv')
+
+
+# 
+# tOverview=d2 %>% arrange(measurement_concept_id,desc(count_value)) %>% 
+# group_by(measurement_concept_id,concept_name.x) %>% 
+#   summarize(n=n()
+#             ,unit_names=paste(concept_name.y,collapse = '|')
+#             ,units_cids=paste(unit_concept_id,collapse = '|')) 
+# tOverview %>% left_join(testsSiteCnt)
+# 
+# 
+# tOverview %<>%   filter(n>1)
+# 
+# tOverviewExt=tOverview %>% left_join(tests) %>%
+#   left_join(testsNtwCnt) %>% 
+#   arrange(desc(site_cnt),desc(network_sum_count_value),measurement_concept_id)
+# names(tOverviewExt)
+# 
+# tOverviewExt  %>% write_csv('ah-tOverviewExt.csv')
